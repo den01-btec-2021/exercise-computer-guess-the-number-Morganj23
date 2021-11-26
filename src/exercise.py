7d6bf859e313a4bb0826a1faf11c59d00b464ae9
21074 <21074@bridgend.ac.uk>
2021-11-26 13:22:15 +0000

    complete

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,27 @@
-def main():
-    #write your code below this line
+from random import randint
 
-if __name__ == '__main__':
-    main()
+print('enter a number between 1 and 10 and the comptuer will guess your number')
+
+number = 0
+
+while number < 1 or number >10:
+    number = int(input('enter a number for the comptuer: '))
+    if number > 10:
+        print('Number must be lower or equal to 10')
+    if number < 1:
+        print('Number must be greater than or qual to 1')
+
+    guess = randint(1,10)
+
+    print(' the comptuer take a guess...', guess)
+
+    while guess != number:
+        if guess > number:
+            guess -= 1 
+            guess = randint(1,guess)
+        else:
+            guess +=1
+            guess = randint(guess, 10)
+            print('the computer takes a guess...', guess)
+
+    print('the comptuter guessed', guess, 'and it was correct!')
